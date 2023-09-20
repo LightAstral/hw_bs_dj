9119295a1e66b23971bc11fac0975fe01cc45b15
LightAstral <140429130+LightAstral@users.noreply.github.com>
2023-09-20 20:14:52 +0300

    dddd

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -2,9 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path("post/", views.post),
-    path("about/", views.about),
-    path("contact/", views.contact),
-    path("services/", views.services),
+    path('blog/', views.index, name="main"),
+    path("blog/post/<int:id>", views.post, name="post"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("services", views.services, name="services"),
+    path('blog/category/<str:name>', views.category, name='category'),
+    path('blog/search/', views.search, name="search"),
+    path('blog/create/', views.create, name="create")
 ]
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('blog/', views.index, name="main"),
+#     path('blog/post/<str:name>/', views.post, name="post"),
+#     path('about/', views.about, name="about"),
+#     path('contact/', views.contact, name="contact"),
+#     path('services/', views.services, name="services"),
+#     path('blog/category/<str:name>/', views.category, name='category')
+# ]
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,6 +1,9 @@
 import random
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
+from django.db.models import Q
+from .forms import PostForm
 
 
 # Create your views here.
@@ -8,12 +11,42 @@ def dummy():
     return str(random.randint(1, 10))
 
 
+def get_categories():
+    all = Category.objects.all()
+    count = all.count()
+    half = count // 2 + 1
+    return {"cat1": all[:half], "cat2": all[half:]}
+
+
 def index(request):
-    return render(request, "blog/index.html")
+    # posts = Post.objects.all()
+    # posts = Post.objects.filter(title__contains='python')
+    # posts = Post.objects.filter(published_date__year=2023)
+    # posts = Post.objects.filter(category__name___iexact='python')
+    posts = Post.objects.order_by('-published_date')
+    # categories = Category.objects.all()
+
+    context = {'posts': posts}
+    context.update(get_categories())
+
+    return render(request, "blog/index.html", context=context)
 
 
-def post(request):
-    return render(request, "blog/post.html")
+def post(request, id=None):
+    post = get_object_or_404(Post, pk=id)
+    context = {"post": post}
+    context.update(get_categories())
+
+    return render(request, "blog/post.html", context=context)
+
+
+def category(request, name=None):
+    c = get_object_or_404(Category, name=name)
+    posts = Post.objects.filter(category=c).order_by('-published_date')
+    context = {"posts": posts}
+    context.update(get_categories())
+
+    return render(request, "blog/index.html", context=context)
 
 
 def about(request):
@@ -26,3 +59,18 @@ def contact(request):
 
 def services(request):
     return render(request, "blog/services.html")
+
+
+def search(request):
+    query = request.GET.get('query')
+    posts = Post.objects.filter(Q(content__icontains=query) | Q(title__icontains=query))
+    context = {"posts": posts}
+    context.update(get_categories())
+
+    return render(request, "blog/index.html", context=context)
+
+
+def create(request):
+    form = PostForm()
+    context = {'form': form}
+    return render(request, "blog/create.html", context=context)
